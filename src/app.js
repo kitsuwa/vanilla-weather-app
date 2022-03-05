1307b7ffc4231b517ed1b14a05b3bdc886d5df75
kitsuwa <99876524+kitsuwa@users.noreply.github.com>
2022-03-06 00:49:51 +0900

    Added 0 in front of hours to create military time format

--- src/app.js
+++ src/app.js
@@ -11,6 +11,9 @@ function formatDate(timestamp) {
   ];
   let day = daysOfWeek[date.getDay()];
   let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
   let minutes = date.getMinutes();
   if (minutes < 10) {
     minutes = `0${minutes}`;

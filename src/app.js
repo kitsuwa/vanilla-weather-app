d09307f6da8d5c70508a4a0a5606e45de76e1a0f
kitsuwa <99876524+kitsuwa@users.noreply.github.com>
2022-03-05 23:44:17 +0900

    Added script to show current temperature and weather for Honolulu

--- src/app.js
+++ src/app.js
@@ -1,1 +1,19 @@
+function displayTemperature(response) {
+  console.log(response.data);
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+}
 
+let apiKey = "b6a37e0dd805d5da5a0b7740137a95f2";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=honolulu&APPID=${apiKey}&units=metric`;
+
+console.log(apiUrl);
+axios.get(apiUrl).then(displayTemperature);
